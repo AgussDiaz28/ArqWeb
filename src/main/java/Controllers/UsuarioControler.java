597491c22671c9c72936e83516f6facd5d7db0d1
Agustin Diaz Gargiulo <agussdiaz28@gmail.com>
2018-11-05 17:35:52 -0300

    web app

---
+++ src/main/java/Controllers/UsuarioControler.java
@@ -0,0 +1,32 @@
+package Controllers;
+
+import javax.persistence.EntityManager;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import Dao.EMF;
+import Dao.UsuarioDAO;
+import Entity.Usuario;
+
+@Path("/usuario")
+public class UsuarioControler {
+	
+	private EntityManager EM;
+	
+	public UsuarioControler(){
+		this.EM = EMF.createEntityManager();	
+	}
+	
+	@GET @Path("/{id}") @Produces(MediaType.APPLICATION_JSON)
+	public String getUserById(@PathParam("id") String msg) {
+		Usuario user = new Usuario();
+//		int id=Integer.valueOf(msg);
+//		Usuario usuario = UsuarioDAO.getInstance().findById(id,this.EM);
+		//return user;
+		return "<html> " + "<title>" + "Hello Jersey" + "</title>";
+	}
+	
+}
