11cb9dbac4f7fe14f256bfeef7d174ab86c5451e
Agustin Diaz Gargiulo <agussdiaz28@gmail.com>
2018-11-13 16:45:01 -0300

    api rest

--- src/main/java/Controllers/UsuarioControler.java
+++ src/main/java/Controllers/UsuarioControler.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import Dao.EMF;
 import Dao.UsuarioDAO;
@@ -21,12 +22,13 @@ public class UsuarioControler {
 	}
 	
 	@GET @Path("/{id}") @Produces(MediaType.APPLICATION_JSON)
-	public String getUserById(@PathParam("id") String msg) {
+	public Response getUserById(@PathParam("id") String msg) {
 		Usuario user = new Usuario();
 //		int id=Integer.valueOf(msg);
 //		Usuario usuario = UsuarioDAO.getInstance().findById(id,this.EM);
 		//return user;
-		return "<html> " + "<title>" + "Hello Jersey" + "</title>";
+		//return "<html> " + "<title>" + "Hello Jersey" + "</title>";
+		return Response.status(201).entity(user).build();
 	}
 	
 }
